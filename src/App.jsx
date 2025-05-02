3f3da3df8c4ea9babf6f7a374eecee4a9ea541f8
Ramandeep <deepraman31104@gmail.com>
2025-05-02 17:48:47 +0530

    2-5-25

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ function App() {
 
   return (
     <>
-    <h1>My react app</h1>
-    <h2>My react app implement with CI/CD</h2>
-    <h3>with guthub actions</h3>
+      <h1>My react app</h1>
+      <h2>My react app implement with CI/CD</h2>
+      <h3>with guthub actions</h3>
+      <p>Lorem, ipsum.</p>
+      <p>Lorem, ipsum.</p>
     </>
   )
 }
